edfb0705d42069a9a5772ab251e8548a4ae3cf3f
tanyapandey200719-netizen <tanyapandey200719@gmail.com>
2025-08-29 23:15:47 +0530

    Create Experiment 1.4.c

---
+++ Experiment 1/Experiment 1.4.c
@@ -0,0 +1,17 @@
+//Write a C program to perform four arthmetic operations on two variables.
+#include<stdio.h>
+int main()
+{
+	int a,b,sum,sub,mul;
+	float div;
+	printf("Enter the number a:");
+	scanf("%d", &a);
+	printf("Enter the number b:");
+	scanf("%d", &b);
+	div=(float)a/b;
+	printf("sum of the numbers is: %d\n",sum=a+b);
+	printf("sub of the numbers is: %d\n",sub=a-b);
+	printf("mul of the numbers is:%d\n",mul=a*b);
+	printf("div of the numbers is:%f\n",div);
+	return 0;
+}
