7687ced755bc139120abccf547b0466a05e25c94
tanyapandey200719-netizen <tanyapandey200719@gmail.com>
2025-09-27 01:31:04 +0530

    Delete ✨ Daily Challenge – 100 Days of Code (UPES) ✨ directory

--- ✨ Daily Challenge – 100 Days of Code (UPES) ✨/Q5 day3.c
+++
@@ -1,9 +0,0 @@
-//convert tempreature from celsius to ferhrenheit 
-#include<stdio.h>
-int main(){
-	double c, f;
-	printf("enter the tempreature in degree celsius:");
-	scanf("%lf",& c);
-	printf("the temperature in fahrenheit is: %lf\n", f = (c * 9/5 )+ 32);
-	return 0;
-}
