fd8922363a6e684694ab48735a9c2ec9be8b6a7a
tanyapandey200719-netizen <tanyapandey200719@gmail.com>
2025-10-25 18:33:15 +0530

    Add files via upload

---
+++ day41/day41q82.c
@@ -0,0 +1,14 @@
+//Q82: Print each character of a string on a new line.
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    fgets(str, sizeof(str), stdin);
+    int i = 0;
+    while(str[i] != '\0' && str[i] != '\n') {
+        printf("%c\n", str[i]);
+        i++;
+    }
+    return 0;
+}
+
