116576de1b11c13a028e43ce3d5dd8fd93b7b065
tanyapandey200719-netizen <tanyapandey200719@gmail.com>
2025-11-19 19:52:32 +0530

    Add files via upload

---
+++ day85/day85q135.c
@@ -0,0 +1,17 @@
+//Assign explicit values starting from 10 and print them.
+#include <stdio.h>
+
+enum Status {
+    SUCCESS = 10,
+    FAILURE,
+    TIMEOUT
+};
+
+int main() {
+    printf("SUCCESS = %d\n", SUCCESS);
+    printf("FAILURE = %d\n", FAILURE);
+    printf("TIMEOUT = %d\n", TIMEOUT);
+
+    return 0;
+}
+
